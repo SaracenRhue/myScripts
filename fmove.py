dbf05d2702c1e403e1e22af1f1b7486d707a295f
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-07-16 19:37:49 +0200

    Add files via upload

---
+++ fmove.py
@@ -0,0 +1,36 @@
+import os
+from os import system as cmd
+from os import listdir as ls
+
+print('Movie(0) or TV Show(1)?')
+category = input()
+
+path = '/mnt/user/Media'
+
+if category == 0:
+    path += '/Movies'
+else: path += '/Serien'
+
+items = ls(path)
+
+for item in items:
+    print(str(items.index(item)) + ') ' + item)
+
+print('Wich one (index)?')
+indexNr = int(input())
+
+path += '/' + items[indexNr]
+
+path = path.replace(' ', '\ ')
+
+destination = '/mnt/user/resilio/Resilio\ Sync'
+
+isFile = os.path.isfile(path)
+
+if isFile:
+    terminal =  'cp ' + path + ' ' + destination
+else: terminal =  'cp -r ' + path + ' ' + destination
+
+cmd(terminal)
+
+print('copying file(s)...')
