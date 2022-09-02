89d3f965d227b99e3c4c70f684feb61b80319ad5
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-02 19:19:37 +0200

    Update codeserver.sh

--- codeserver.sh
+++ codeserver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt update -y
-apt upgrade -y
-apt remove -y python
-apt install -y g++ zsh zsh-autosuggestions zsh-syntax-highlighting build-essential procps curl file git curl
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt remove -y python
+sudo apt install -y g++ zsh zsh-autosuggestions zsh-syntax-highlighting build-essential procps curl file git curl
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
