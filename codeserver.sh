feb3c2fc58d8a9498749b5dc021a9d8cb8867f2c
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-02 19:03:01 +0200

    Create codeserver.sh

---
+++ codeserver.sh
@@ -0,0 +1,12 @@
+
+
+apt update -y
+apt upgrade -y
+apt remove -y python
+apt install -y g++ zsh zsh-autosuggestions zsh-syntax-highlighting build-essential procps curl file git curl
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install htop python nodejs npm openjdk kotlin go rust
+
+source ~/.bashrc
