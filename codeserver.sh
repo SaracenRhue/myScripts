86e50e48e4f4ff644ee9dc30f64758165e88a2f7
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-20 12:07:12 +0200

    Update codeserver.sh

--- codeserver.sh
+++ codeserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#sudo apt update -y
-#sudo apt upgrade -y
-#sudo apt remove -y python
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt remove -y python
 sudo apt install build-essential procps file -y
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 #echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
 
