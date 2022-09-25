5531b085e3e3308ecd9acfd6b4b2cb703ceec09d
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-25 23:18:08 +0200

    Create cust_vsc.sh

---
+++ cust_vsc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt remove -y python
+sudo apt install -y build-essential procps file
+
+touch ~/.bashrc
+
+
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+
+source ~/.bashrc
+brew install python nodejs npm openjdk kotlin go rust
+source ~/.bashrc
+npm install -g typescript
+npm install -g sass
+pip3 install requests selenium pygame pyautogui pyyaml
