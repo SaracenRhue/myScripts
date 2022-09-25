2ededa7a03f1257071181009b33e50ac26f65822
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-26 00:06:40 +0200

    Update cust_vsc.sh

--- cust_vsc.sh
+++ cust_vsc.sh
@@ -6,7 +6,7 @@ apt remove -y python
 apt install -y build-essential procps file
 
 
-yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes ' ' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
 
