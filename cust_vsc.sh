3ab1b27773ca1096ac81f0a622ba7fb1d7cc8ac1
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-10-09 12:57:42 +0200

    Update cust_vsc.sh

--- cust_vsc.sh
+++ cust_vsc.sh
@@ -16,3 +16,12 @@ npm install -g typescript
 npm install -g sass
 pip3 install requests selenium pygame pyautogui pyyaml Pillow python-opencv
 
+sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting
+echo "plugins=(zsh-autosuggestions)" >> ~/.zshrc
+sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+curl -L http://install.ohmyz.sh | sh
+chsh -s $(which zsh)
+echo "zsh" >> ~/.bashrc
+source ~/.bashrc
+source ~/.zshrc
