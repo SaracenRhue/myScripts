cf408ded8cae9c562d027f63421519df071a27ba
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-10-31 17:01:44 +0100

    Update alpine_ios.sh

--- alpine_ios.sh
+++ alpine_ios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apk add update && apk add upgrade
-apk add python3 py3-pip nodejs npm firefox-esr
+apk add bash git curl wget python3 py3-pip nodejs npm firefox-esr
 pip3 install numpy pyyaml selenium
