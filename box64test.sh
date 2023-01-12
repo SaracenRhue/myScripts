2e26349b2a4129c3f7d2819520ebb7f8e6b09b1b
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-01-12 23:08:21 +0100

    Update box64test.sh

--- box64test.sh
+++ box64test.sh
@@ -2,5 +2,5 @@
 
 wget https://factorio.com/get-download/stable/headless/linux64 -O factorio.tar.xz
 tar -xJf factorio.tar.xz
-./factorio/bin/x64/factorio --create ./factorio/saves/my-save.zip
-./factorio/bin/x64/factorio --start-server ./factorio/saves/my-save.zip
+/home/pi/factorio/bin/x64/factorio --create ./factorio/saves/my-save.zip
+/home/pi/factorio/bin/x64/factorio --start-server ./factorio/saves/my-save.zip
