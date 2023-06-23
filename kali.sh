1d588c56f69665a8f5d9da1df3843d2d0688fae3
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-06-23 23:35:47 +0200

    u

--- kali.sh
+++ kali.sh
@@ -7,7 +7,7 @@ echo "neofetch" >> ~/.zshrc
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 echo "export NIXPKGS_ALLOW_UNFREE=1" >> ~/.zshrc
-source ~/.zshrc
+export NIXPKGS_ALLOW_UNFREE=1
 nix-env -iA nixpkgs.vscode
 echo "set root password"
 sudo passwd root
