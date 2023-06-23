096addf57b0d25eee7fa61a12213c82fbb6d4fb4
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-06-24 00:21:37 +0200

    u

--- kali.sh
+++ kali.sh
@@ -2,14 +2,18 @@
 
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y wget curl neofetch python3-pip nodejs npm htop 
+sudo apt install -y wget curl neofetch python3-pip nodejs npm htop terminator
 echo "neofetch" >> ~/.zshrc
+echo "alias 'python'='python3'" >> ~/.zshrc
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 echo "export NIXPKGS_ALLOW_UNFREE=1" >> ~/.zshrc
-export NIXPKGS_ALLOW_UNFREE=1
-nix-env -iA nixpkgs.vscode
+source ~/.zshrc
 echo "set root password"
 sudo passwd root
+
+
+# nix-env -iA nixpkgs.vscode
+
 
 
