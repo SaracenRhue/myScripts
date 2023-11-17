9567da935840c6ec94275858ff43a09e959142b6
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-17 13:07:53 +0100

    Update casaServer.sh

--- casaServer.sh
+++ casaServer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl wget git htop
 curl -fsSL https://get.casaos.io | sudo bash
 curl -fsSL https://tailscale.com/install.sh | sh
