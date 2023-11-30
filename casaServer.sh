7df645620d1f372a682db594529e977771e0db9d
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-30 10:40:30 +0100

    Update casaServer.sh

--- casaServer.sh
+++ casaServer.sh
@@ -4,4 +4,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl wget git htop
 curl -fsSL https://get.casaos.io | sudo bash
 curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+sudo tailscale up --advertise-exit-node
